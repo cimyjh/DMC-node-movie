cef34e3b2ccf0f0c2852ffc47c076ca4807d62eb
cimyjh@gmail.com <developer123@yjh>
2020-07-28 21:39:12 +0900

    movie Main OK

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = "/api/users";
+export const API_URL = "https://api.themoviedb.org/3/";
+export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+
+export const API_KEY = "06cc0dce691a2310f6b20f232d7da363";
---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from "../../Config";
+import MainImage from "./Sections/MainImage";
+import GridCards from "../commons/GridCards";
+import { Row } from "antd";
+
+// API를 통해 데이터를 가져오는 것 이기에 URL parser을 한다.
+// State를 사용하는 패턴
+
+function LandingPage() {
+  //state
+  const [Movies, setMovies] = useState([]);
+  const [MainMovieImage, setMainMovieImage] = useState(null);
+  const [CurrentPage, setCurrentPage] = useState(0);
+
+  useEffect(() => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    fetchMovies(endpoint);
+  }, []);
+
+  const fetchMovies = (endpoint) => {
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        //가져온 객체가 누적이 된다.
+        setMovies([...Movies, ...response.results]);
+        setMainMovieImage(response.results[0]);
+        setCurrentPage(response.page);
+      });
+  };
+
+  const loadMoreItems = () => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${
+      CurrentPage + 1
+    }`;
+    fetchMovies(endpoint);
+  };
+
+  return (
+    <div style={{ width: "100%", margin: "0" }}>
+      {/* 메인 대문 이미지 */}
+      {/* 메인이미지가 있으면 랜더링 하기, 이미지 정보를 가지고온 다음 렌더링 되도록 설정한 것  */}
+
+      {MainMovieImage && (
+        <MainImage
+          image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+          title={MainMovieImage.original_title}
+          text={MainMovieImage.overview}
+        />
+      )}
+
+      <div style={{ width: "85", margin: "1rem auto" }}>
+        {/* 콘텐츠 그리드 부분 */}
+        <h2>Movies By latest</h2>
+        <hr />
+
+        <Row gutter={[16, 16]}>
+          {Movies &&
+            Movies.map((movie, index) => (
+              <React.Fragment key={index}>
+                <GridCards
+                  landingPage
+                  image={
+                    movie.poster_path
+                      ? `${IMAGE_BASE_URL}w500${movie.poster_path}`
+                      : null
+                  }
+                  movieId={movie.id}
+                  movieName={movie.original_title}
+                />
+              </React.Fragment>
+            ))}
+        </Row>
+      </div>
+
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button onClick={loadMoreItems}> Load More</button>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
